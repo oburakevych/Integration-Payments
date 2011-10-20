1733ec1bcef0e3419a0505330f9c1683b69d4c96
nvolynets <naz.mymail@gmail.com>
2011-10-20 12:35:14 +0300

    Created project of payments service which will responsible for all incoming requests from tradshift payments client & all request will be tracked in DB & this service will include integration with all payments services (dips, moneybooker & etc.)

---
+++ payments-service/src/main/java/org/integration/payments/service/App.java
@@ -0,0 +1,13 @@
+package org.integration.payments.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
