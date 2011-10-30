d0ae79c2d77eae51dbe40a41d8e2e42de41f3d18
Alexander Burakevych <abu@tradeshift.com>
2011-10-30 19:10:30 +0100

    Added XPath parsers.

--- payments-service/src/main/java/org/integration/payments/server/util/IOUtils.java
+++ payments-service/src/main/java/org/integration/payments/server/util/IOUtils.java
@@ -1,10 +1,21 @@
 package org.integration.payments.server.util;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-public class IOUtils {
-    public static byte[] getResourceAsByteArray(InputStream is) throws IOException {
-        return org.apache.commons.io.IOUtils.toByteArray(is);
+public class IOUtils extends org.apache.commons.io.IOUtils {
+    public static byte[] getResourceAsByteArray(String path) throws IOException {
+        InputStream is = new FileInputStream(path);
+        
+        return toByteArray(is);
     }
+    
+    public static InputStream toInputStream(File file) throws IOException {
+        InputStream is = new FileInputStream(file);
+        return is;
+    }
+    
+    
 }
--- payments-service/src/test/java/org/integration/payments/server/document/DocumentServiceTest.java
+++ payments-service/src/test/java/org/integration/payments/server/document/DocumentServiceTest.java
@@ -10,9 +10,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 import oasis.names.specification.ubl.schema.xsd.invoice_2.InvoiceType;
 import oasis.names.specification.ubl.schema.xsd.invoice_2.ObjectFactory;
 
+import org.integration.payments.server.document.ubl.canonical.Canonical;
 import org.integration.payments.server.util.IOUtils;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -20,6 +25,9 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+import static org.junit.Assert.*;
 
 
 @ContextConfiguration(locations={"/test-root-context.xml"})
@@ -27,24 +35,28 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 public class DocumentServiceTest {
     @Autowired DocumentService documentService;
     
-    public void document_can_be_validated() throws IOException {
-
+    @Test
+    public void document_can_be_validated() throws IOException, SAXException, ParserConfigurationException {
+        Document xmlDoc = getXmlDomDocumentRepresentation("src/test/resources/ubl_invoice.xml");
+        
+        assertEquals("12701", Canonical.NUMBER.getUblString(xmlDoc));
+        assertEquals("5002701", Canonical.BUYERS_ORDER_ID.getUblString(xmlDoc));
+        assertEquals("Tradeshift", Canonical.RECEIVER_PARTY_NAME.getUblString(xmlDoc));
+        assertEquals("DK", Canonical.RECEIVER_COUNTRY_CODE.getUblString(xmlDoc));
+        assertEquals("250.00", Canonical.PAYABLE_AMOUNT.getUblString(xmlDoc));
+        assertEquals("DKK", Canonical.CURRENCY_CODE.getUblString(xmlDoc));
     }
     
     @Test
     public void document_can_be_converted_to_invoice() throws IOException {
-        InputStream is = new FileInputStream("src/test/resources/TSUBL_Invoice_01.xml");
-        
-        byte[] xml = IOUtils.getResourceAsByteArray(is);
+        byte[] xml = IOUtils.getResourceAsByteArray("src/test/resources/ubl_invoice.xml");
         
         InvoiceType invoice = documentService.convertToInvoice(xml);
     }
     
     @Test
     public void document_can_be_converted_to_xml() throws IOException {
-        InputStream is = new FileInputStream("src/test/resources/TSUBL_Invoice_01.xml");
-        
-        byte[] xml = IOUtils.getResourceAsByteArray(is);
+        byte[] xml = IOUtils.getResourceAsByteArray("src/test/resources/ubl_invoice.xml");
         
         InvoiceType invoice = documentService.convertToInvoice(xml);
         
@@ -77,5 +89,14 @@ public class DocumentServiceTest {
     
     public void application_response_can_be_validated() {
         
+    }
+    
+    private Document getXmlDomDocumentRepresentation(String path) throws ParserConfigurationException, SAXException, IOException {
+        DocumentBuilderFactory newInstance = DocumentBuilderFactory.newInstance();
+        newInstance.setNamespaceAware(true);
+        DocumentBuilder builder = newInstance.newDocumentBuilder();
+        Document doc = builder.parse(IOUtils.toInputStream(new File("src/test/resources/ubl_invoice.xml")));
+        
+        return doc;
     }
 }
